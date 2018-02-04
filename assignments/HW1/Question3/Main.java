cf97f387e2fbadc9eeaf04c31a6c5e31bc43bc18
Alex Loyko <alex.loyko96@gmail.com>
2018-02-03 21:24:35 -0500

    Question 3 added

---
+++ assignments/HW1/Question3/Main.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+import javafx.beans.property.ReadOnlyDoubleWrapper;
+
+public class Main {
+    public static void main(String [] args) {
+        String [] months = {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        };
+
+        Random rand = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int randomMonth = rand.nextInt(12) + 1;
+            System.out.println("Our random month this time is " + months[randomMonth-1]);
+        }
+    }
+}
